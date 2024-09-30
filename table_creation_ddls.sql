885e8b42935181f72b0ca5e2501096f9e127989e
MandarAcharekar <mandarmva8055@gmail.com>
2024-09-30 20:32:53 +0530

    table creation ddls

---
+++ table_creation_ddls.sql
@@ -0,0 +1,44 @@
+CREATE TABLE Theatres (
+    theatre_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE Movies (
+    movie_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    language VARCHAR(50),
+    rating CHAR(2),
+    format VARCHAR(10)
+);
+
+
+CREATE TABLE Shows (
+    show_id INT PRIMARY KEY AUTO_INCREMENT,
+    theatre_id INT,
+    movie_id INT,
+    date DATE,
+    time TIME,
+    availability BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY (theatre_id) REFERENCES Theatres(theatre_id),
+    FOREIGN KEY (movie_id) REFERENCES Movies(movie_id)
+);
+
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(15)
+);
+
+
+CREATE TABLE Bookings (
+    booking_id INT PRIMARY KEY AUTO_INCREMENT,
+    show_id INT,
+    customer_id INT,
+    number_of_tickets INT NOT NULL,
+    FOREIGN KEY (show_id) REFERENCES Shows(show_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
